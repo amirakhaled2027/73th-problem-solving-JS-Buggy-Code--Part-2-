afd66c2ae01abc1bc9454f58684897c524519e84
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-08-07 22:10:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+//Fix the code in the code tab to pass this challenge (only syntax errors).
+
+// function maxNum(n1;n2) {
+// 	if (n1>n2) {
+// 	  return n2
+// 	}
+//   else if {
+// 	return n1
+//   }
+// }
+
+
+function maxNum(n1, n2) {
+	if (n1> n2) {
+	  return n1
+	}
+    else {
+        return n2
+    }
+}
+console.log(maxNum(3, 7));
+console.log(maxNum(-1, 0));
+console.log(maxNum(1000, 400));
